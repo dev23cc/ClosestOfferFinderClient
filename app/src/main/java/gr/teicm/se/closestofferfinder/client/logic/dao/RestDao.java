51cacb24b31cd7e6be8a12d674b554b5e9caccac
dev23cc <dev23cc@yandex.com>
2014-11-21 15:39:31 +0200

    some comments in restdao

--- app/src/main/java/gr/teicm/se/closestofferfinder/client/logic/dao/RestDao.java
+++ app/src/main/java/gr/teicm/se/closestofferfinder/client/logic/dao/RestDao.java
@@ -5,6 +5,7 @@ package gr.teicm.se.closestofferfinder.client.logic.dao;
 public class RestDao {
     RestDao() {
     //    HttpClient client = HttpClientBuilder.create().build();
-
+    // http://83.212.101.78:8080/WSoffer/service/getAllOffersXML
+    // http://83.212.101.78:8080/WSoffer/service/getAllOffersJSON
     }
 }
